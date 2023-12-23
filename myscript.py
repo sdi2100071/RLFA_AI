c7ef9fd5f703165e6ca47c683a0fe6fb616b6af6
sdi2100071 <mikaelakullolli123@gmail.com>
2023-12-23 19:54:35 +0200

    neighbors

--- myscript.py
+++ myscript.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     
     for i in range(1, len(temp)):
         variables.append(temp[i].split(" ")[0])
+    
    
     path = "C:\\Users\\mikae\\OneDrive\\Υπολογιστής\\5th_semmester\\AI\\RLFA_AI\\rlfap\\dom2-f24.txt"
     with open(path, 'r') as dom:
@@ -34,8 +35,60 @@ if __name__ == "__main__":
         for j in range(1, len(doms[i].split(" "))):
             vars.append(doms[i].split(" ")[j])
         domains[key] = vars
+        
+    domains = {int(key):[int(i) for i in val]
+       for key, val in domains.items()}
+
+         
     # print(domains)
     # print(variables)
+    
+    path = "C:\\Users\\mikae\\OneDrive\\Υπολογιστής\\5th_semmester\\AI\\RLFA_AI\\rlfap\\ctr2-f24.txt"
+    with open(path, 'r') as cons:
+        lines = cons.readlines()
         
+    # print(lines)
+    temp= []
+    for line in lines:
+        asList = line.split(", ")
+        temp.append(asList[0])
+    
+    neighbors = {key: [] for key in variables}    
+    for i in range(1, len(temp)):
+        key = temp[i].split(" ")[0]
+        neighbors[key].append(temp[i].split(" ")[1])
+        neighbors[temp[i].split(" ")[1]].append(key)
+        
+   
+    variables  = list(map(int, variables)) 
+    neighbors = {int(key):[int(i) for i in val]
+       for key, val in neighbors.items()}
+    print(neighbors)
+    
+    
+    
+    
+    
+    
+    
+    
+    #     vars[i - 1] = vars[i - 1] + [temp[i].split(" ")[1]]
+    #     print(vars[i - 1])
+    #     # print( temp[i].split(" ")[1])
+    #     neighbors[key] = vars[i - 1]
+    # print(neighbors)
+    
+    # alist = []
+    # neighbors = dict.fromkeys(range(1, len(temp)), alist)
+    # # alist.append(2)
+    # for key in [temp[i].split(" ")[0] for i in range(1, len(temp))]:        
+        
+    
+    
+        
+    
+    # print(temp)
+    
+            
         
     
