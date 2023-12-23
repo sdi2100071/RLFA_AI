95a3914151b7dde80419c4ad25f1b8a00a2b6e76
sdi2100071 <mikaelakullolli123@gmail.com>
2023-12-23 22:48:24 +0200

    constraints

--- myscript.py
+++ myscript.py
@@ -1,5 +1,20 @@
 import csp
 import csv
+import operator
+
+def constraints(A, a, B, b, neighbors, cons):
+
+    ind = neighbors[A].index(B)
+    cond = cons[A][ind]    
+    print(ind)   
+    ops = { "=": operator.eq, ">": operator.gt } 
+    sub = abs(a - b)
+    res = int(cond[1])
+    if ops[cond[0]](sub, res):
+        return True
+    else:
+        return False   
+    
 
 if __name__ == "__main__":
     
@@ -49,16 +64,24 @@ if __name__ == "__main__":
         asList = line.split(", ")
         temp.append(asList[0])
     
-    neighbors = {key: [] for key in variables}    
+    neighbors = {key: [] for key in variables} 
+    cons = {key: [] for key in variables}   
     for i in range(1, len(temp)):
         key = temp[i].split(" ")[0]
         neighbors[key].append(temp[i].split(" ")[1])
+        cons[key].append([temp[i].split(" ")[2], temp[i].split(" ")[3].replace("\n","")])
         neighbors[temp[i].split(" ")[1]].append(key)
+        cons[temp[i].split(" ")[1]].append([temp[i].split(" ")[2], temp[i].split(" ")[3].replace("\n","")])
         
+    
+    b = constraints('2', 1, '1' , 86, neighbors, cons)
+    print(b)
+    
     variables  = list(map(int, variables)) 
     neighbors = {int(key):[int(i) for i in val]
        for key, val in neighbors.items()}
-    print(neighbors)
+    
+    # print(temp[1].split(" ")[3])
     
     
             
