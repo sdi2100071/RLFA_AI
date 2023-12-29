55dfc4f681aa38d7ac21dc8e7342be554cd36db2
sdi2100071 <mikaelakullolli123@gmail.com>
2023-12-29 23:29:20 +0200

    fc + mac

--- rlfa.py
+++ rlfa.py
@@ -85,3 +85,27 @@ class rlfa(csp.CSP):
         return constraint_propagation(csp, {(X, var) for X in csp.neighbors[var]}, removals)
                 
             
+def Fc_Cbj(csp, select_unassigned_variable=first_unassigned_variable,
+                        order_domain_values=unordered_domain_values, inference=no_inference):
+
+    def Cbj(assignment):
+        if len(assignment) == len(csp.variables):
+            return assignment
+        var = select_unassigned_variable(assignment, csp)
+        for value in order_domain_values(var, assignment, csp):
+            if 0 == csp.nconflicts(var, value, assignment):
+                csp.assign(var, value, assignment)
+                removals = csp.suppose(var, value)
+                if inference(csp, var, value, assignment, removals):
+                    result = Cbj(assignment)
+                    if result is not None:
+                        return result
+                csp.restore(removals)
+        csp.unassign(var, assignment)
+        return None
+
+    result = Cbj({})
+    assert result is None or csp.goal_test(result)
+    return result
+
+
